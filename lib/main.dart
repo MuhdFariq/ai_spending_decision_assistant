c5a417c4167ed74ed9c5187d830bfcc89c5e4792
MuhdFariq <fariqdude@gmail.com>
2026-04-25 16:15:23 +0800

    Final Pushed

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:ai_spending_decision_assistant/screens/affordability_checker_scr
 import 'package:ai_spending_decision_assistant/screens/ai_chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'firebase_options.dart';
-import 'services/analytics_service.dart';
 import 'screens/home_shell.dart';
 import 'screens/add_expense_screen.dart';
 import 'screens/home_screen.dart';
@@ -18,16 +16,11 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key, this.home, this.enableAnalytics = true});
 
-  static final AnalyticsService analyticsService = AnalyticsService();
   final Widget? home;
   final bool enableAnalytics;
 
   @override
   Widget build(BuildContext context) {
-    final List<NavigatorObserver> navigatorObservers = enableAnalytics
-        ? <NavigatorObserver>[analyticsService.getAnalyticsObserver()]
-        : <NavigatorObserver>[];
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'AI Spending Decision Assistant',
@@ -36,7 +29,6 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFFF6F3FB),
         useMaterial3: true,
       ),
-      navigatorObservers: navigatorObservers,
       home: home ?? const HomeShell(),
     );
   }
